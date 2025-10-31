d6b9af16475618bb7a3fb2b1aad587a6795cdc0a
ShauryaStar-ai <shauryahooda2009@gmail.com>
2025-10-31 18:05:59 -0400

    The prompt is now ready to be send to the llm in the message_placeholder

---
+++ RefrenceProjects/MessagePlacHolder/message_placeholder.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
+import os
+
+API_KEY = os.getenv("OPEN_AI_API_KEY")
+model = ChatOpenAI(openai_api_key=API_KEY, max_tokens=50)
+
+# Define a chat template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful Amazon Customer Service Assistant."),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{input}")
+])
+
+# Load chat history
+chat_history = []
+with open("chat.txt", "r") as f:
+    for line in f:
+        chat_history.append(HumanMessage(content=line.strip()))
+
+# Invoke the prompt
+res = prompt.invoke({
+    "history": chat_history,
+    "input": "When will the refund be there?"
+})
+
+print(res)
